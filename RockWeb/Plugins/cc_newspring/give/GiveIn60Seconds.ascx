c034acfff3b08eddbf683e7f76e7d96492b68fbe
David Stevens <david.stevens@newspring.cc>
2014-06-01 19:10:21 -0400

    Weekend changes

--- RockWeb/Plugins/cc_newspring/give/GiveIn60Seconds.ascx
+++ RockWeb/Plugins/cc_newspring/give/GiveIn60Seconds.ascx
@@ -1,1 +1,121 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="GiveIn60Seconds.ascx.cs" Inherits="Plugins_cc_newspring_give_GiveIn60Seconds" %>
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="GiveIn60Seconds.ascx.cs" Inherits="RockWeb.Plugins.cc_newspring.give.GiveIn60Seconds" %>
+
+<asp:UpdatePanel ID="pnlContent" runat="server" UpdateMode="Conditional">
+<ContentTemplate>
+    <asp:Panel ID="pnlHeader" runat="server">
+        <Rock:BootstrapButton ID="btnBack" runat="server" Text="BACK" OnClick="btnBack_Click" />
+    </asp:Panel>
+
+    <asp:UpdatePanel ID="pnlBody" runat="server" UpdateMode="Conditional">
+    <ContentTemplate>
+        
+        <asp:ValidationSummary ID="lblWarning" runat="server" />
+
+        <asp:Panel ID="pnlStepOne" runat="server">
+            <form class="form-horizontal">
+                <asp:Repeater ID="rptAccountList" runat="server">
+                    <ItemTemplate>
+                        <Rock:CurrencyBox ID="txtAccountAmount" runat="server" Label='<%# Eval("Name") %>' Text='<%# Eval("AmountFormatted") %>' Placeholder="0.00" CssClass="account-amount" />
+                    </ItemTemplate>
+                </asp:Repeater>
+            </form>
+
+            <div class="form-group">
+                <label>Total</label>
+                <asp:Label ID="lblTotalAmount" runat="server" CssClass="form-control-static total-amount" />
+            </div>
+
+            <div id="divRepeatingPayments" runat="server" visible="false">
+                <Rock:ButtonDropDownList ID="btnFrequency" runat="server" CssClass="btn btn-primary" Label="Frequency"
+                    DataTextField="Name" DataValueField="Id" />
+                <Rock:DatePicker ID="dtpStartDate" runat="server" Label="First Payment" />
+            </div>
+        
+        </asp:Panel>
+
+        <asp:Panel ID="pnlStepTwo" runat="server" Visible="false">         
+
+            <Rock:PhoneNumberBox ID="txtNumber" runat="server" Label="Phone" />
+            <Rock:RockCheckBox ID="chkPerson" runat="server" />
+            <Rock:RockTextBox ID="txtEmail" runat="server" />            
+            <Rock:CampusPicker ID="cpCampus" runat="server" Label="Your Campus"/>
+
+        
+        </asp:Panel>
+
+        <asp:Panel ID="pnlStepThree" runat="server" Visible="false">
+        
+            <fieldset>
+
+            <Rock:RockTextBox ID="txtFirstName" runat="server" Label="First Name" />
+            <Rock:RockTextBox ID="txtLastName" runat="server" Label="Last Name" />
+            <Rock:RockTextBox ID="txtStreet" runat="server" Label="Address" /> 
+            <Rock:RockTextBox Label="City" ID="txtCity" runat="server" />
+            <Rock:StateDropDownList Label="State" ID="ddlState" runat="server" UseAbbreviation="true" />
+            <Rock:RockTextBox  ID="txtZip" runat="server" Label="Zip" />
+            
+            <div id="divCCPaymentInfo" runat="server">
+                <fieldset>
+                    <Rock:RockRadioButtonList ID="rblSavedCC" runat="server" Label=" " CssClass="radio-list" RepeatDirection="Vertical" DataValueField="Id" DataTextField="Name" />
+                    <div id="divNewCard" runat="server" class="radio-content">
+                        <Rock:RockTextBox ID="txtCardFirstName" runat="server" Label="First Name on Card" Visible="false"></Rock:RockTextBox>
+                        <Rock:RockTextBox ID="txtCardLastName" runat="server" Label="Last Name on Card" Visible="false"></Rock:RockTextBox>
+                        <Rock:RockTextBox ID="txtCardName" runat="server" Label="Name on Card" Visible="false"></Rock:RockTextBox>
+                        <Rock:RockTextBox ID="txtCreditCard" runat="server" Label="Credit Card #" MaxLength="19" CssClass="credit-card" />
+                        <ul class="card-logos">
+                            <li class="card-visa"></li>
+                            <li class="card-mastercard"></li>
+                            <li class="card-amex"></li>
+                            <li class="card-discover"></li>
+                        </ul>
+                        <Rock:MonthYearPicker ID="mypExpiration" runat="server" Label="Expiration Date" />
+                        <Rock:NumberBox ID="txtCVV" Label="Card Security Code" runat="server" MaxLength="4" />                        
+                    </div>
+                </fieldset>
+            </div>
+
+            <div id="divACHPaymentInfo" runat="server">
+                <fieldset>
+                    <Rock:RockRadioButtonList ID="rblSavedAch" runat="server" Label=" " CssClass="radio-list" RepeatDirection="Vertical" DataValueField="Id" DataTextField="Name" />
+                    <div id="divNewBank" runat="server" class="radio-content">                                    
+                        <Rock:RockTextBox ID="txtBankName" runat="server" Label="Bank Name" />
+                        <Rock:RockTextBox ID="txtRoutingNumber" runat="server" Label="Routing #" />
+                        <Rock:RockTextBox ID="txtAccountNumber" runat="server" Label="Account #" />
+                        <Rock:RockRadioButtonList ID="rblAccountType" runat="server" RepeatDirection="Horizontal" Label="Account Type">
+                            <asp:ListItem Text="Checking" Selected="true" />
+                            <asp:ListItem Text="Savings" />
+                        </Rock:RockRadioButtonList>
+                        <asp:Image ID="imgCheck" runat="server" ImageUrl="<%$ Fingerprint:~/Assets/Images/check-image.png %>" />                                    
+                    </div>
+                </fieldset>
+            </div>
+
+            
+        
+        </asp:Panel>
+
+        <asp:Panel ID="pnlStepFour" runat="server" Visible="false">
+        
+
+
+        
+        </asp:Panel>
+
+        <Rock:BootstrapButton ID="btnNext" runat="server" Text="NEXT" OnClick="btnNext_Click" />
+
+    </ContentTemplate>
+    </asp:UpdatePanel>
+
+
+    <asp:Panel ID="pnlFooter" runat="server">
+    
+
+
+
+    
+    </asp:Panel>
+
+    <asp:HiddenField ID="hfCurrentPage" runat="server" />
+        
+</ContentTemplate>
+</asp:UpdatePanel>
--- RockWeb/Plugins/cc_newspring/give/GiveIn60Seconds.ascx.cs
+++ RockWeb/Plugins/cc_newspring/give/GiveIn60Seconds.ascx.cs
@@ -1,14 +1,200 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class Plugins_cc_newspring_give_GiveIn60Seconds : System.Web.UI.UserControl
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Financial;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.cc_newspring.give
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    #region Block Attributes
 
+    /// <summary>
+    /// Add a new one-time or scheduled transaction
+    /// </summary>
+    [DisplayName( "Give In 60 Seconds" )]
+    [Category( "NewSpring > Give In 60 Seconds" )]
+    [Description( "Provides a simple giving page to look up attendee data and process contributions." )]
+    [ComponentField( "Rock.Financial.GatewayContainer, Rock", "Credit Card Gateway", "The payment gateway to use for Credit Card transactions", false, "", "", 0, "CCGateway" )]
+    [ComponentField( "Rock.Financial.GatewayContainer, Rock", "ACH Card Gateway", "The payment gateway to use for ACH (bank account) transactions", false, "", "", 1, "ACHGateway" )]
+    [TextField( "Batch Name Prefix", "The batch prefix name to use when creating a new batch", false, "Online Giving - ", "", 2 )]
+    [DefinedValueField( Rock.SystemGuid.DefinedType.FINANCIAL_SOURCE_TYPE, "Source", "The Financial Source Type to use when creating transactions", false, false, "", "", 3 )]
+    [GroupLocationTypeField( Rock.SystemGuid.GroupType.GROUPTYPE_FAMILY, "Address Type", "The location type to use for the person's address", false,
+        Rock.SystemGuid.DefinedValue.GROUP_LOCATION_TYPE_HOME, "", 4 )]
+    [BooleanField( "Scheduled Transactions", "Allow", "Don't Allow",
+        "If the selected gateway(s) allow scheduled transactions, should that option be provided to user", true, "", 9, "AllowScheduled" )]
+    [CodeEditorField( "Confirmation Header", "The text (HTML) to display at the top of the confirmation section.", CodeEditorMode.Html, CodeEditorTheme.Rock, 400, true, @"
+<p>
+Please confirm the information below. Once you have confirmed that the information is accurate click the 'Finish' button to complete your transaction.
+</p>
+", "Text Options", 13 )]
+    [CodeEditorField( "Confirmation Footer", "The text (HTML) to display at the bottom of the confirmation section.", CodeEditorMode.Html, CodeEditorTheme.Rock, 400, true, @"
+<div class='alert alert-info'>
+By clicking the 'finish' button below I agree to allow {{ OrganizationName }} to debit the amount above from my account. I acknowledge that I may
+update the transaction information at any time by returning to this website. Please call the Finance Office if you have any additional questions.
+</div>
+", "Text Options", 14 )]
+    [CodeEditorField( "Success Header", "The text (HTML) to display at the top of the success section.", CodeEditorMode.Html, CodeEditorTheme.Rock, 400, true, @"
+<p>
+Thank you for your generous contribution.  Your support is helping {{ OrganizationName }} actively
+achieve our mission.  We are so grateful for your commitment.
+</p>
+", "Text Options", 15 )]
+    [CodeEditorField( "Success Footer", "The text (HTML) to display at the bottom of the success section.", CodeEditorMode.Html, CodeEditorTheme.Rock, 400, true, @"
+", "Text Options", 16 )]
+    [EmailTemplateField( "Confirm Account", "Confirm Account Email Template", false, Rock.SystemGuid.SystemEmail.SECURITY_CONFIRM_ACCOUNT, "Email Templates", 17, "ConfirmAccountTemplate" )]
+
+    #endregion
+
+    public partial class GiveIn60Seconds : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        private GatewayComponent _ccGateway;
+
+        private GatewayComponent _achGateway;
+
+        /// <summary>
+        /// Gets or sets the accounts that are available for user to add to the list.
+        /// </summary>
+        protected List<AccountItem> AvailableAccounts
+        {
+            get
+            {
+                var accounts = ViewState["AvailableAccounts"] as List<AccountItem>;
+                if ( accounts == null )
+                {
+                    accounts = new List<AccountItem>();
+                }
+
+                return accounts;
+            }
+
+            set
+            {
+                ViewState["AvailableAccounts"] = value;
+            }
+        }
+
+        #endregion
+
+        #region Base Control Methods
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                var rockContext = new RockContext();
+
+                foreach ( var account in new FinancialAccountService( rockContext ).Queryable()
+                .Where( f =>
+                    f.IsActive &&
+                    f.PublicName != null &&
+                    f.PublicName.Trim() != string.Empty &&
+                    ( f.StartDate == null || f.StartDate <= RockDateTime.Today ) &&
+                    ( f.EndDate == null || f.EndDate >= RockDateTime.Today ) )
+                .OrderBy( f => f.Order ) )
+                {
+                    var accountItem = new AccountItem( account.Id, account.Order, account.Name, account.CampusId );
+                    AvailableAccounts.Add( accountItem );
+                }
+
+                rptAccountList.DataSource = AvailableAccounts.OrderBy( a => a.Order ).ToList();
+                rptAccountList.DataBind();
+                //cpCampus.SetValue( gfSettings.GetUserPreference( "Campus" ) );
+
+                //BindGrid();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the Click event of the btnNext control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnNext_Click( object sender, EventArgs e )
+        {
+            //// Page 1 = Payment Info
+            //// Page 2 = Confirmation
+            //// Page 3 = Success
+            //// Page 0 = Only message box is displayed
+
+            int pageId = hfCurrentPage.Value.AsInteger() ?? 0;
+            if ( pageId > 0 )
+            {
+                pnlStepOne.Visible = pageId == 1;
+                pnlStepTwo.Visible = pageId == 2;
+                pnlStepThree.Visible = pageId == 3;
+                //divActions.Visible = page > 0;
+
+                btnBack.Visible = pageId == 2;
+                btnNext.Visible = pageId < 3;
+                btnNext.Text = pageId > 1 ? "Finish" : "Next";
+            }
+
+            hfCurrentPage.Value = pageId.ToString();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnBack control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnBack_Click( object sender, EventArgs e )
+        {
+        }
+
+        #endregion
+
+        #region Helper Classes
+
+        /// <summary>
+        /// Lightweight object for each contribution item
+        /// </summary>
+        [Serializable]
+        protected class AccountItem
+        {
+            public int Id { get; set; }
+
+            public int Order { get; set; }
+
+            public string Name { get; set; }
+
+            public int? CampusId { get; set; }
+
+            public decimal Amount { get; set; }
+
+            public string AmountFormatted
+            {
+                get
+                {
+                    return Amount > 0 ? Amount.ToString( "F2" ) : string.Empty;
+                }
+            }
+
+            public AccountItem( int id, int order, string name, int? campusId )
+            {
+                Id = id;
+                Order = order;
+                Name = name;
+                CampusId = campusId;
+            }
+        }
+
+        #endregion
     }
 }
