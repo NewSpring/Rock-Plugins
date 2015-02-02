a7f7d4788e98f39ce0c7ebe4d2c923e71d56d04b
dcs619 <dcs619@gmail.com>
2015-02-02 13:54:14 -0500

    Fix error on null Type

--- cc.newspring.Apollos/Workflow/Action/APISync.cs
+++ cc.newspring.Apollos/Workflow/Action/APISync.cs
@@ -17,10 +17,12 @@
      * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
      */
 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using Rock.Attribute;
+using Rock.Data;
 using Rock.Model;
 
 namespace cc.newspring.Apollos.Workflow.Action
@@ -28,7 +30,7 @@ namespace cc.newspring.Apollos.Workflow.Action
     [Description( "Sync with API" )]
     [Export( typeof( Rock.Workflow.ActionComponent ) )]
     [ExportMetadata( "ComponentName", "API Sync" )]
-    [TextField( "Sync URL", "The URL endpoint to synchronize with", false, "" )]
+    [TextField( "Sync URL", "The specific URL endpoint this related entity type should synchronize with", false, "" )]
     [TextField( "Token Name", "The key by which the token should be identified in the header of HTTP requests", false, "" )]
     [TextField( "Token Value", "The value of the token to authenticate with the URL endpoint", false, "" )]
     public class APISync : Rock.Workflow.ActionComponent
