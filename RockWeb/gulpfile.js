5e8c173544ad7ac73a948b15ea8358bd7571e60b
dcs619 <dcs619@gmail.com>
2015-03-01 13:06:18 -0500

    Added gulp compilation back to ns-themes

---
+++ RockWeb/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	less = require('gulp-less');
+
+gulp.task('less', function () {
+  return gulp.src('Themes/KidSpring/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('Themes/KidSpring'))
+});
+
+gulp.task('sass', function(){
+	gulp.src('Themes/KidSpring/Styles/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('Themes/KidSpring/Styles'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch(['Themes/**/*.scss','Plugins/cc_newspring/**/*.scss'], ['sass']);
+	gulp.watch('Themes/**/*.less', ['less']);
+});
+
+gulp.task('default', ['less','sass','watch']);
