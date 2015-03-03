77d73a99d4aa1f3afface098413fc36e49ff45e2
benwiley <ben.wiley@newspring.cc>
2015-03-03 15:50:28 -0500

    Add delete and update endpoints

--- cc.newspring.Apollos/Rest/Controllers/ApollosUserLoginsController.cs
+++ cc.newspring.Apollos/Rest/Controllers/ApollosUserLoginsController.cs
@@ -25,7 +25,7 @@ namespace cc.newspring.Apollos.Rest.Controllers
         [HttpGet]
         [Authenticate, Secured]
         [System.Web.Http.Route( "api/UserLogins/{id}" )]
-        public new ApollosUserLogin GetById( int id )
+        public ApollosUserLogin GetById( int id )
         {
             return new ApollosUserLogin( base.GetById( id ) );
         }
@@ -37,7 +37,7 @@ namespace cc.newspring.Apollos.Rest.Controllers
         [HttpGet]
         [Authenticate, Secured]
         [System.Web.Http.Route( "api/UserLogins" )]
-        public new object[] Get()
+        public object[] Get()
         {
             var logins = base.Get();
             var apollosLogins = new Queue();
@@ -50,6 +50,95 @@ namespace cc.newspring.Apollos.Rest.Controllers
             return apollosLogins.ToArray();
         }
 
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        [HttpDelete]
+        [Authenticate, Secured]
+        [System.Web.Http.Route( "api/UserLogins/{id}" )]
+        public void Delete( int id )
+        {
+            base.Delete( id );
+        }
+
+        /// <summary>
+        /// Updates the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authenticate, Secured]
+        [System.Web.Http.Route( "api/UserLogins/{id}" )]
+        public HttpResponseMessage Update( int id, [FromBody]Dictionary<string, string> data )
+        {
+            var response = new HttpResponseMessage();
+            var context = new RockContext();
+            var userLoginService = new UserLoginService( context );
+            var userLogin = userLoginService.Get( id );
+            var changes = false;
+
+            if ( userLogin == null )
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.Content = new StringContent( string.Format( "There is no UserLogin that employs id {0}", id ) );
+                return response;
+            }
+
+            if ( data.ContainsKey( UserNameKey ) )
+            {
+                var userName = data[UserNameKey];
+
+                if ( !Validation.IsEmail( userName ) )
+                {
+                    response.StatusCode = HttpStatusCode.BadRequest;
+                    response.Content = new StringContent( string.Format( "{0} must be a valid email", UserNameKey ) );
+                    return response;
+                }
+
+                var existing = userLoginService.GetByUserName( userName );
+
+                if ( existing != null && existing.Id != id )
+                {
+                    response.StatusCode = HttpStatusCode.Conflict;
+                    response.Content = new StringContent( string.Format( "UserLogin with id={0} employs username {1}", existing.Id, existing.UserName ) );
+                    return response;
+                }
+
+                changes = true;
+                userLogin.UserName = userName;
+            }
+
+            if ( data.ContainsKey( HashKey ) )
+            {
+                var hash = data[HashKey];
+
+                if ( !Validation.IsBcryptHash( hash ) )
+                {
+                    response.StatusCode = HttpStatusCode.BadRequest;
+                    response.Content = new StringContent( string.Format( "{0} must be a valid Bcrypt hash", HashKey ) );
+                    return response;
+                }
+
+                changes = true;
+                userLogin.Password = hash;
+            }
+
+            if ( changes )
+            {
+                context.SaveChanges();
+            }
+
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
+        }
+
+        /// <summary>
+        /// Inserts the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
         [HttpPost]
         [Authenticate, Secured]
         [System.Web.Http.Route( "api/UserLogins" )]
