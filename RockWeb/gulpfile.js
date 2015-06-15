11c85ad71b82d19afbe928660d2c8f8496b2fb52
briankalwat <brian@briankalwat.com>
2015-06-15 14:02:27 -0400

    deleted minified files, updated gulpfile

--- RockWeb/gulpfile.js
+++ RockWeb/gulpfile.js
@@ -3,20 +3,28 @@ var gulp = require('gulp'),
 	less = require('gulp-less');
 
 gulp.task('less', function () {
-  return gulp.src('./Themes/NewSpring/**/*.less')
+  	return gulp.src([
+  		'./Themes/NewSpring/**/*.less',
+  		'!./Themes/NewSpring/**/_*.less'
+  	])
     .pipe(less())
     .pipe(gulp.dest('./Themes/NewSpring'))
 });
 
 gulp.task('sass', function(){
-	gulp.src('./Plugins/**/*.scss')
+	gulp.src([
+		'./Themes/NewSpring/**/*.scss',
+  		'!./Themes/NewSpring/**/_*.scss'
+	])
 	.pipe(sass())
-	.pipe(gulp.dest('./Plugins'))
+	.pipe(gulp.dest('./Themes/NewSpring'))
 });
 
 gulp.task('watch', function(){
-	gulp.watch(['Themes/**/*.scss','Plugins/cc_newspring/**/*.scss'], ['sass']);
-	gulp.watch('Themes/**/*.less', ['less']);
+	gulp.watch('Themes/NewSpring/**/*.scss', ['sass']);
+	gulp.watch('Themes/NewSpring/**/*.less', ['less']);
 });
+
+gulp.task('build', ['less','sass']);
 
 gulp.task('default', ['less','sass','watch']);
