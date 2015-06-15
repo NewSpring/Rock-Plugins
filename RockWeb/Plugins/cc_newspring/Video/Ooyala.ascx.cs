661dc4bc8ae3d1eb6c9fc71c5b22420e5c6fb712
edolyne <ed@edolyne.com>
2015-06-15 10:31:41 -0400

    Work on ooyala block type

---
+++ RockWeb/Plugins/cc_newspring/Video/Ooyala.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.cc_newspring.Video
+{
+    [TextField( "Ooyala Content ID", "Paste the Ooyala Content ID here" )]
+    public partial class Ooyala : Rock.Web.UI.RockBlock
+    {
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            // Set the ooyala id
+            ooyalaId.Value = GetAttributeValue( "OoyalaContentID" );
+        }
+    }
+}
