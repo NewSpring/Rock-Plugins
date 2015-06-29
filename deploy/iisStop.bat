1d0a71b3ab0e21c74bbc77427309c1047e241b2f
richarddubay <richardtdubayjr@gmail.com>
2015-06-29 15:31:18 -0400

    first attempt at checking to see if the w3svc is running before trying to stop it.

--- deploy/iisStop.bat
+++ deploy/iisStop.bat
@@ -1,1 +1,11 @@
-net stop w3svc
+REM query to see if the w3svc service is running
+sc query w3svc
+
+REM if the w3svc is running then it should return a state of 4
+echo STATE
+
+REM only if the w3svc is running do we want to try to stop it.
+REM if we try to stop it when it is already stopped it will throw an error.
+if STATE == 4 (
+  net stop w3svc
+)
