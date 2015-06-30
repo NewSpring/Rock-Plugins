b0fb6b89450fa8d13137ee6b8384b968649d3558
richarddubay <richardtdubayjr@gmail.com>
2015-06-29 22:33:30 -0400

    clean up both scripts. hoping this works now.

--- deploy/iisStart.bat
+++ deploy/iisStart.bat
@@ -1,13 +1,8 @@
-REM query to see if the w3svc service is running
+REM query to see if the w3svc service is running.
 sc query w3svc | find "RUNNING"
-
-REM if the w3svc is running then it should return a state of 4
-REM echo %STATE%
-echo %ERRORLEVEL%
 
 REM only if the w3svc is not running do we want to try to start it.
 REM if we try to start it when it is already started it will throw an error.
-REM if NOT %STATE% == 4 (
 if NOT "%ERRORLEVEL%"=="0" (
   net start w3svc
 )
