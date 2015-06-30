6536fb58d9394468d3f1652b008101d93155f2d2
richarddubay <richardtdubayjr@gmail.com>
2015-06-29 21:30:36 -0400

    trying to find the right way to get the return STATE value.

--- deploy/iisStart.bat
+++ deploy/iisStart.bat
@@ -1,11 +1,11 @@
 REM query to see if the w3svc service is running
-sc query w3svc
+sc query w3svc | find "RUNNING"
 
 REM if the w3svc is running then it should return a state of 4
-echo STATE
+echo %STATE%
 
 REM only if the w3svc is not running do we want to try to start it.
 REM if we try to start it when it is already started it will throw an error.
-if NOT STATE == 4 (
+if NOT %STATE% == 4 (
   net start w3svc
 )
