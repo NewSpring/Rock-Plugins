a18e6f7710fbd1475881afa34987b6ccb86ffb2a
edolyne <hello@edolyne.com>
2015-07-09 17:59:41 -0400

    ROCK-442 Work on optimization for Week of Year searches

--- RockWeb/Plugins/cc_newspring/Metrics/Metrics.ascx.cs
+++ RockWeb/Plugins/cc_newspring/Metrics/Metrics.ascx.cs
@@ -17,6 +17,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web.UI;
@@ -26,6 +27,7 @@ using Rock.Attribute;
 using Rock.Data;
 using Rock.Model;
 using Rock.Reporting.Dashboard;
+using Rock.Utility;
 using Rock.Web.Cache;
 using Rock.Web.UI;
 using Rock.Web.UI.Controls;
@@ -43,7 +45,6 @@ namespace RockWeb.Plugins.cc_newspring.Metrics
     [CustomDropdownListField( "Metric Period", "", "This Week,Last Week,One Year Ago,YTD", DefaultValue = "YTD", Order = 3 )]
     [MetricCategoriesField( "Metric Source", "Select the metric to include in this chart.", false, "", "", 4 )]
     // [SlidingDateRangeField( "Date Range", Key = "SlidingDateRange", DefaultValue = "1||4||", Order = 7 )]
-    // [LinkedPage( "Detail Page", "Select the page to navigate to when the chart is clicked", Order = 8 )]
     public partial class Metrics : Rock.Web.UI.RockBlock
     {
         #region Control Methods
@@ -67,112 +68,120 @@ namespace RockWeb.Plugins.cc_newspring.Metrics
             // Show the warning if no data is selected
             churchMetricWarning.Visible = !churchMetricSource.Any();
 
-            var newMetric = new MetricService( new RockContext() ).GetByIds( churchMetricSource );
+            var newMetric = new MetricService( new RockContext() ).GetByIds( churchMetricSource ).ToArray()[0].MetricValues;
 
             if ( GetAttributeValue( "MetricDisplayType" ) == "Text" )
             {
-                foreach ( var metric in newMetric )
+                var churchMetricValue = newMetric;
+
+                //foreach ( var metric in newMetric )
+                //{
+                //    var churchMetricValue = metric.MetricValues;
+
+                if ( churchMetricPeriod == "YTD" )
                 {
-                    var churchMetricValue = metric.MetricValues;
-                    var sortedMetric = churchMetricValue
-                        .OrderByDescending( c => c.MetricValueDateTime )
-                        .ToArray();
+                    metricNumber.Value = string.Format( "{0:n0}", churchMetricValue.Where( a => a.MetricValueDateTime > new DateTime( DateTime.Now.Year, 1, 1 ) ).Select( a => a.YValue ).Sum() );
+                }
+                else
+                {
+                    var calendar = DateTimeFormatInfo.CurrentInfo.Calendar;
+
+                    // Current Week of Year
+                    var currentWeekOfYear = calendar.GetWeekOfYear( DateTime.Now, CalendarWeekRule.FirstDay, DayOfWeek.Sunday );
+
+                    // Last Week
+                    var lastWeekOfYear = calendar.GetWeekOfYear( DateTime.Now.AddDays( -7 ), CalendarWeekRule.FirstDay, DayOfWeek.Sunday );
+
+                    // Search DB Based on Current Week of Year
+                    var currentWeekMetric = churchMetricValue.Where( a => calendar.GetWeekOfYear( a.MetricValueDateTime.Value.Date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday ) == currentWeekOfYear && a.MetricValueDateTime.Value.Year == DateTime.Now.Year ).Select( a => a.YValue ).ToArray();
+
+                    // Search DB Based on Last Week
+                    var lastWeekMetric = churchMetricValue.Where( a => calendar.GetWeekOfYear( a.MetricValueDateTime.Value.Date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday ) == lastWeekOfYear && a.MetricValueDateTime.Value.Year == DateTime.Now.Year ).Select( a => a.YValue ).ToArray();
 
                     if ( churchMetricPeriod == "This Week" )
                     {
-                        var currentWeek = sortedMetric[0].YValue;
-                        var lastWeek = sortedMetric[1].YValue;
+                        // This Week Metric Value
+                        metricNumber.Value = string.Format( "{0:n0}", currentWeekMetric[0] );
 
-                        if ( currentWeek > lastWeek )
+                        // Get The CSS Classes For The Trend Arrow
+                        if ( currentWeekMetric[0] > lastWeekMetric[0] )
                         {
                             metricClass.Value = "fa-caret-up brand-success";
-                            metricNumber.Value = string.Format( "{0:n0}", currentWeek );
                         }
                         else
                         {
                             metricClass.Value = "fa-caret-down brand-danger";
-                            metricNumber.Value = string.Format( "{0:n0}", currentWeek );
                         }
                     }
                     else if ( churchMetricPeriod == "Last Week" )
                     {
-                        var lastWeek = sortedMetric[1].YValue;
-
-                        metricNumber.Value = string.Format( "{0:n0}", lastWeek );
+                        // Last Week Metric Value
+                        metricNumber.Value = string.Format( "{0:n0}", lastWeekMetric[0] );
                     }
                     else if ( churchMetricPeriod == "One Year Ago" )
                     {
-                        // Search the DB
-                        var lastYear = churchMetricValue
-                            .Where( a => a.MetricValueDateTime > DateTime.Now.AddYears( -1 ).AddDays( -7 ) && a.MetricValueDateTime < DateTime.Now.AddYears( -1 ).AddDays( 1 ) )
-                            .Select( a => a.YValue )
-                            .ToArray();
+                        // Search DD For Metric From This Week Last Year
+                        var lastYearMetric = churchMetricValue.Where( a => calendar.GetWeekOfYear( a.MetricValueDateTime.Value.Date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday ) == currentWeekOfYear && a.MetricValueDateTime.Value.Year == DateTime.Now.AddYears( -1 ).Year ).Select( a => a.YValue ).ToArray();
 
-                        // Output the value
-                        metricNumber.Value = string.Format( "{0:n0}", lastYear[0] );
+                        // This Week Last Year Metric
+                        metricNumber.Value = string.Format( "{0:n0}", lastYearMetric[0] );
                     }
-                    else
-                    {
-                        var ytdSince = new DateTime( DateTime.Now.Year, 1, 1 );
-
-                        var metricSum = churchMetricValue.Where( a => a.MetricValueDateTime > ytdSince ).Select( a => a.YValue ).ToArray();
-                        metricNumber.Value = string.Format( "{0:n0}", metricSum.Sum() );
-                    }
+                    // }
                 }
             }
             else if ( GetAttributeValue( "MetricDisplayType" ) == "Line" )
             {
-                foreach ( var metric in newMetric )
-                {
-                    var churchMetricValue = metric.MetricValues;
+                //foreach ( var metric in newMetric )
+                //{
+                var churchMetricValue = newMetric;
 
-                    currentYear.Value = DateTime.Now.Year.ToString();
-                    previousYear.Value = DateTime.Now.AddYears( -1 ).Year.ToString();
+                currentYear.Value = DateTime.Now.Year.ToString();
+                previousYear.Value = DateTime.Now.AddYears( -1 ).Year.ToString();
 
-                    // Create an array of of labels
-                    var metricLabelsArray = churchMetricValue.Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -42 ) )
-                        .OrderBy( a => a.MetricValueDateTime )
-                        .Select( a => new DateTime( a.MetricValueDateTime.Value.Year, a.MetricValueDateTime.Value.Month, a.MetricValueDateTime.Value.Day ).ToString( "MMMM dd" ) )
-                        .ToArray();
+                // Create an array of of labels
+                var metricLabelsArray = churchMetricValue.Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -42 ) )
+                    .OrderBy( a => a.MetricValueDateTime )
+                    .Select( a => new DateTime( a.MetricValueDateTime.Value.Year, a.MetricValueDateTime.Value.Month, a.MetricValueDateTime.Value.Day ).ToString( "MMMM dd" ) )
+                    .ToArray();
 
-                    var metricLabelsString = string.Join( ",", metricLabelsArray );
+                var metricLabelsString = string.Join( ",", metricLabelsArray );
 
-                    // Format the array of labels for output
-                    metricLabels.Value = "'" + metricLabelsString.Replace( ",", "','" ) + "'";
+                // Format the array of labels for output
+                metricLabels.Value = "'" + metricLabelsString.Replace( ",", "','" ) + "'";
 
-                    // Create an array of data points (Current Year)
-                    //var metricDataPointSumsCurrentYear = churchMetricValue
-                    //    .Where( a => a.MetricValueDateTime > DateTime.Now.AddMonths( -6 ) )
-                    //    .Select( a => new { Month = a.MetricValueDateTime.Value.Month, YValue = a.YValue } )
-                    //    .GroupBy( a => a.Month )
-                    //    .Select( a => new { a.Key, Sum = a.Sum( v => v.YValue ).ToString() } )
-                    //    .OrderBy( a => a.Key )
-                    //    .Select( a => a.Sum)
-                    //    .ToArray();
+                // Create an array of data points (Current Year)
+                //var metricDataPointSumsCurrentYear = churchMetricValue
+                //    .Where( a => a.MetricValueDateTime > DateTime.Now.AddMonths( -6 ) )
+                //    .Select( a => new { Month = a.MetricValueDateTime.Value.Month, YValue = a.YValue } )
+                //    .GroupBy( a => a.Month )
+                //    .Select( a => new { a.Key, Sum = a.Sum( v => v.YValue ).ToString() } )
+                //    .OrderBy( a => a.Key )
+                //    .Select( a => a.Sum)
+                //    .ToArray();
 
-                    var metricDataPointSumsCurrentYear = churchMetricValue
-                        .Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -42 ) )
-                        .OrderBy( a => a.MetricValueDateTime )
-                        .Select( a => string.Format( "{0:0}", a.YValue ) )
-                        .ToArray();
+                var metricDataPointSumsCurrentYear = churchMetricValue
+                    .Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -42 ) )
+                    .OrderBy( a => a.MetricValueDateTime )
+                    .Select( a => string.Format( "{0:0}", a.YValue ) )
+                    .ToArray();
 
-                    var metricDataPointStringCurrent = string.Join( ",", metricDataPointSumsCurrentYear );
+                var metricDataPointStringCurrent = string.Join( ",", metricDataPointSumsCurrentYear );
 
-                    // Format the array of sums for output
-                    metricDataPointsCurrent.Value = "'" + metricDataPointStringCurrent.Replace( ",", "','" ) + "'";
+                // Format the array of sums for output
+                metricDataPointsCurrent.Value = "'" + metricDataPointStringCurrent.Replace( ",", "','" ) + "'";
 
-                    // Create an array of data points (Current Year)
-                    var metricDataPointSumsPreviousYear = churchMetricValue
-                        .Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -1 ).AddMonths( -42 ) && a.MetricValueDateTime < DateTime.Now.AddYears( -1 ) )
-                        .OrderBy( a => a.MetricValueDateTime )
-                        .Select( a => string.Format( "{0:0}", a.YValue ) )
-                        .ToArray();
+                // Create an array of data points (Current Year)
+                var metricDataPointSumsPreviousYear = churchMetricValue
+                    .Where( a => a.MetricValueDateTime > DateTime.Now.AddDays( -1 ).AddMonths( -42 ) && a.MetricValueDateTime < DateTime.Now.AddYears( -1 ) )
+                    .OrderBy( a => a.MetricValueDateTime )
+                    .Select( a => string.Format( "{0:0}", a.YValue ) )
+                    .ToArray();
 
-                    var metricDataPointStringPrevious = string.Join( ",", metricDataPointSumsPreviousYear );
+                var metricDataPointStringPrevious = string.Join( ",", metricDataPointSumsPreviousYear );
 
-                    // Format the array of sums for output
-                    metricDataPointsPrevious.Value = "'" + metricDataPointStringPrevious.Replace( ",", "','" ) + "'";
-                }
+                // Format the array of sums for output
+                metricDataPointsPrevious.Value = "'" + metricDataPointStringPrevious.Replace( ",", "','" ) + "'";
+                //}
             }
         }
 
