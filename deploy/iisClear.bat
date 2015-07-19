619ca98d564795a250d62f48f543a785bf7714ca
dcs619 <dcs619@gmail.com>
2015-07-19 12:05:13 -0400

    Remove circle files

--- deploy/iisClear.bat
+++
@@ -1,5 +0,0 @@
-copy C:\inetpub\wwwroot\web.connectionstrings.config C:\inetpub\temp\web.connectionstrings.config
-del /s /q C:\inetpub\wwwroot\*
-copy C:\inetpub\temp\web.connectionstrings.config C:\inetpub\wwwroot\web.connectionstrings.config
-
-
--- deploy/iisStop.bat
+++
@@ -1,8 +0,0 @@
-REM query to see if the w3svc service is running.
-sc query w3svc | find "RUNNING"
-
-REM only if the w3svc is running do we want to try to stop it.
-REM if we try to stop it when it is already stopped it will throw an error.
-if "%ERRORLEVEL%"=="0" (
-  net stop w3svc
-)
