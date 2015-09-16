9f86f6146d8b9d7619a1c0954b69bbc3f6127fbb
dcs619 <dcs619@gmail.com>
2015-09-16 10:23:18 -0400

    Added the metrics migration project

---
+++ cc.newspring.Metrics/Migrations/AddSQLTypes.cs
@@ -0,0 +1,46 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Plugin;
+
+namespace cc.newspring.Metrics.Migrations
+{
+    [MigrationNumber( 1, "1.3.4" )]
+    public class AddSQLTypes : Migration
+    {
+        protected string MinistryMetricGuid = "A5F29054-EC70-4BA3-B181-E2A62D11A929";
+
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+        }
+    }
+}
