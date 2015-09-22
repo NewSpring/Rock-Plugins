ef315bd7fe7c2d45c329ca3c2e77c80b0eb4a210
dcs619 <dcs619@gmail.com>
2015-09-22 10:59:12 -0400

    excluded old defined type migration

---
+++ cc.newspring.Metrics/Migrations/AddMetricTypes.cs
@@ -0,0 +1,52 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rock;
+using Rock.Data;
+using Rock.Plugin;
+
+/// <summary>
+/// Adds the queries needed for the Ministry Metric block
+/// </summary>
+namespace cc.newspring.Metrics.Migrations
+{
+    [MigrationNumber( 1, "1.3.4" )]
+    public class AddMetricTypes : Migration
+    {
+        // Defined  variables
+
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+        }
+    }
+}
