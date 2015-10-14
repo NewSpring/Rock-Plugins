3ababc52a62dc9fc92cf3132f29c28ac12beb119
dcs619 <dcs619@gmail.com>
2015-10-14 10:59:29 -0400

    ROCK-820

--- Dev Tools/Sql/Populate_NewSpring_Metrics.sql
+++ Dev Tools/Sql/Populate_NewSpring_Metrics.sql
@@ -1,124 +1,31 @@
+/* ====================================================== 
+-- NewSpring Populate Metrics: 
+-- Populates all metrics with values
+  
+--  Assumptions:
+--  Values only calculated for the previous Sunday. {Total} 
+--  metrics will receive one value, while {Service} metrics 
+--  will receive values for each service.
+
+   ====================================================== */
+-- Make sure you're using the right Rock database:
+
+--USE Rock
+
+SET NOCOUNT ON
+
+DECLARE @msg nvarchar(255)
 DECLARE @IsSystem bit = 0
 DECLARE @True bit = 1
 DECLARE @False bit = 0
 DECLARE @Order int = 0
-DECLARE @BooleanFieldTypeId int
-DECLARE @GroupEntityTypeId int
-DECLARE @CampusEntityTypeId int
-DECLARE @ScheduleEntityTypeId int
-DECLARE @MetricCategoryEntityTypeId int
-DECLARE @CheckInAreaPurposeId int
-DECLARE @CampusLocationTypeId int
-DECLARE @SourceTypeSQLId int
-DECLARE @MetricGroupTypeCategoryId int
-DECLARE @MetricServiceRolesId int
-DECLARE @CampusId int, @GroupId int
-DECLARE @GroupName varchar(255)
+DECLARE @CampusId int
+DECLARE @MetricName nvarchar(255)
+DECLARE @MetricDescription nvarchar(255)
+DECLARE @MetricId int
 
-SELECT TOP 1 @CampusId = [Id] FROM [Campus]
-SELECT @BooleanFieldTypeId = [Id] FROM [FieldType] WHERE [Name] = 'Boolean'
-SELECT @GroupEntityTypeId = [Id] FROM [EntityType] WHERE [Name] = 'Rock.Model.Group'
-SELECT @CampusEntityTypeId = [Id] FROM [EntityType] WHERE [Name] = 'Rock.Model.Campus'
-SELECT @ScheduleEntityTypeId = [Id] FROM [EntityType] WHERE [Name] = 'Rock.Model.Schedule'
-SELECT @MetricCategoryEntityTypeId = [Id] FROM [EntityType] WHERE [Name] = 'Rock.Model.MetricCategory'
-
-DECLARE @MetricParentCategoryId int, @MetricScheduleCategoryId int, 
-	@MetricScheduleId int, @MetricParentName varchar(255), 
-	@MetricScheduleCategory varchar(255), @MetriciCalSchedule nvarchar(max)
-
-SELECT @MetricParentName = 'Volunteer Groups', @MetricScheduleCategory = 'Metrics'
-
--- Pick a volunteer group to assign metrics to
-SELECT TOP 1 @GroupId = G.[Id], @GroupName = G.Name
-FROM [Group] G
-INNER JOIN GroupType GT
-	ON G.GroupTypeId = GT.Id
-	AND GT.name = 'Next Steps Volunteer'
-	AND G.ParentGroupId IS NOT NULL
-
--- create parent category
-SELECT @MetricParentCategoryId = [Id] FROM [Category]
-WHERE EntityTypeId = @MetricCategoryEntityTypeId
-AND Name = @MetricParentName
-
-IF @MetricParentCategoryId IS NULL 
-BEGIN
-	INSERT [Category] (IsSystem, ParentCategoryId, EntityTypeId, EntityTypeQualifierColumn, EntityTypeQualifierValue, Name, [Guid], [Order])
-	VALUES ( @IsSystem, NULL, @MetricCategoryEntityTypeId, '', '', @MetricParentName, NEWID(), @Order )
-
-	SET @MetricParentCategoryId = SCOPE_IDENTITY()
-END
-
-SELECT @MetricScheduleCategoryId = [Id] FROM [Category]
-WHERE EntityTypeId = @ScheduleEntityTypeId
-AND Name = @MetricScheduleCategory
-
--- create schedule category
-IF @MetricScheduleCategoryId IS NULL
-BEGIN
-	INSERT [Category] (IsSystem, ParentCategoryId, EntityTypeId, EntityTypeQualifierColumn, EntityTypeQualifierValue, Name, [Guid], [Order])
-	VALUES ( @IsSystem, NULL, @ScheduleEntityTypeId, '', '', @MetricScheduleCategory, NEWID(), @Order )
-
-	SET @MetricScheduleCategoryId = SCOPE_IDENTITY()
-END
-
--- create the metric schedule
-SELECT @MetricScheduleId = [Id] FROM Schedule
-WHERE CategoryId = @MetricScheduleCategoryId
-AND Name = @MetricParentName
-
-IF @MetricScheduleId IS NULL
-BEGIN
-
-	SELECT @MetriciCalSchedule = N'BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//ddaysoftware.com//NONSGML DDay.iCal 1.0//EN
-BEGIN:VEVENT
-DTEND:20150928T020001
-DTSTAMP:20150928T201239Z
-DTSTART:20150928T020000
-RRULE:FREQ=WEEKLY;BYDAY=MO
-SEQUENCE:0
-UID:4bb6f790-4761-447d-bff8-22c2ca3bef05
-END:VEVENT
-END:VCALENDAR'
-	
-	INSERT [Schedule] (Name, [Description], iCalendarContent, EffectiveStartDate, EffectiveEndDate, CategoryId, [Guid])
-	SELECT 'Metric Schedule', 'The job schedule to run group metrics', @MetriciCalSchedule, GETDATE(), GETDATE(), @MetricScheduleCategoryId, NEWID()
-
-	SELECT @MetricScheduleId = SCOPE_IDENTITY()
-
-END
-
-SELECT @MetricGroupTypeCategoryId = [Id] FROM Category
-WHERE ParentCategoryId = @MetricParentCategoryId
-AND Name = 'Next Steps'
-
-IF @MetricGroupTypeCategoryId IS NULL
-BEGIN
-	INSERT [Category] (IsSystem, ParentCategoryId, EntityTypeId, EntityTypeQualifierColumn, EntityTypeQualifierValue, Name, [Guid], [Order])
-	VALUES ( @IsSystem, @MetricParentCategoryId, @MetricCategoryEntityTypeId, '', '', 'Next Steps', NEWID(), @Order )
-
-	SELECT @MetricGroupTypeCategoryId = SCOPE_IDENTITY()
-END
-
-SELECT @MetricServiceRolesId = [Id] FROM Metric
-WHERE EntityTypeId = @CampusEntityTypeId
-	AND SourceValueTypeId = @SourceTypeSQLId
-	AND Title = @GroupName
-
--- create if it doesn't exist
-IF @MetricServiceRolesId IS NULL
-BEGIN
-	INSERT [Metric] (IsSystem, Title, [Description], IsCumulative, SourceValueTypeId, SourceSql, XAxisLabel, YAxisLabel, ScheduleId, EntityTypeId, [Guid], ForeignId)
-	VALUES ( 0, @GroupName + ' Service Roles', 'Metric to track ' + @GroupName + ' roles by campus and service', @False, 
-		@SourceTypeSQLId, 'This is where the SQL would go', '', '', @MetricScheduleId, @CampusEntityTypeId, NEWID(), @GroupId  )
-
-	SELECT @MetricServiceRolesId = SCOPE_IDENTITY()
-
-	INSERT [MetricCategory] (MetricId, CategoryId, [Order], [Guid])
-	VALUES ( @MetricServiceRolesId, @MetricGroupTypeCategoryId, @Order, NEWID() )
-END
+-- pick the first campus in the db
+SELECT TOP 1 @CampusId = [Id] FROM [Campus]	
 
 DECLARE @9am varchar(5) = '09:15'
 DECLARE @11am varchar(5) = '11:15'
@@ -130,27 +37,69 @@ DECLARE @LastSunday datetime = DATEADD(
 	DAY, -((DATEPART(DW, GETDATE()) + 6) % 7), GETDATE()
 )
 
-DECLARE @scopeIndex int, @numItems int		
-SELECT @scopeIndex = 0
-SELECT @numItems = 52
+-- don't randomize the date for now
+--SELECT @SomeSunday = DATEADD(dd, DATEDIFF(dd, @scopeIndex * 7, @LastSunday), 0)
 
-WHILE @scopeIndex <= @numItems
+SELECT @SomeSunday = @LastSunday
+
+DECLARE @scopeIndex int, @numItems int, @numSundays int = 52
+SELECT @scopeIndex = min(Id) FROM Metric
+SELECT @numItems = count(1) + @scopeIndex FROM Metric
+
+WHILE @scopeIndex < @numItems
 BEGIN
-
-	SELECT @SomeSunday = DATEADD(dd, DATEDIFF(dd, @scopeIndex * 7, @LastSunday), 0)
-
-	INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
-	VALUES (@MetricServiceRolesId, @False, CAST(ABS(RAND() * 1000) AS INT), @Order, @SomeSunday + @9am, @CampusId, NEWID() )
-
-	INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
-	VALUES (@MetricServiceRolesId, @False, CAST(ABS(RAND() * 1000) AS INT), @Order, @SomeSunday + @11am, @CampusId, NEWID() )
 	
-	INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
-	VALUES (@MetricServiceRolesId, @False, CAST(ABS(RAND() * 1000) AS INT), @Order, @SomeSunday + @4pm, @CampusId, NEWID() )
+	SELECT @MetricId = @scopeIndex
 
-	INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
-	VALUES (@MetricServiceRolesId, @False, CAST(ABS(RAND() * 1000) AS INT), @Order, @SomeSunday + @6pm, @CampusId, NEWID() )
+	-- log the description since a lot of metrics have the same name
+	SELECT @MetricName = Title, @MetricDescription = SUBSTRING(Description, 17, LEN(Description) - 16)
+	FROM Metric
+	WHERE Id = @MetricId
+
+	IF @MetricName <> ''
+	BEGIN
+		
+		SELECT @msg = 'Creating values for ' + @MetricDescription
+		RAISERROR ( @msg, 0, 0 ) WITH NOWAIT
+
+		-- create a random, single, large number for totals or uniques
+		IF @MetricName LIKE '%Total%' OR @MetricName LIKE '%Unique%'
+		BEGIN
+
+			INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
+			VALUES (@MetricId, @False, CAST(ABS(RAND() * 1000) AS INT), @Order, @SomeSunday + '00:00', @CampusId, NEWID() )
+
+		END
+		-- create random, smaller numbers for each service time
+		ELSE BEGIN
+
+			INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
+			VALUES (@MetricId, @False, CAST(ABS(RAND() * 100) AS INT), @Order, @SomeSunday + @9am, @CampusId, NEWID() )
+
+			INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
+			VALUES (@MetricId, @False, CAST(ABS(RAND() * 100) AS INT), @Order, @SomeSunday + @11am, @CampusId, NEWID() )
+	
+			INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
+			VALUES (@MetricId, @False, CAST(ABS(RAND() * 100) AS INT), @Order, @SomeSunday + @4pm, @CampusId, NEWID() )
+
+			INSERT [MetricValue] (MetricId, MetricValueType, YValue, [Order], MetricValueDateTime, EntityId, [Guid])
+			VALUES (@MetricId, @False, CAST(ABS(RAND() * 100) AS INT), @Order, @SomeSunday + @6pm, @CampusId, NEWID() )	
+
+		END
+
+	END
+	ELSE BEGIN
+		SELECT @msg = 'Could not find metric ' + @MetricName
+		RAISERROR ( @msg, 0, 0 ) WITH NOWAIT
+	END
 	
 	SELECT @scopeIndex = @scopeIndex + 1
-		
 END
+
+
+
+
+
+
+	
+		
